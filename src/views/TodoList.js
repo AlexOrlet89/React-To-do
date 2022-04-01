40ddfd2514cf945bafe2070da47f762dd62706f2
Alex Orlet <AlexOrlet89@gmail.com>
2022-04-01 14:31:53 -0700

    added todo form and started handlesubmit, needs another fetch

--- src/views/TodoList.js
+++ src/views/TodoList.js
@@ -4,6 +4,7 @@ import { getTodos } from '../services/todos';
 
 export default function TodoList({ currentUser }) {
   const [todos, setTodos] = useState([]);
+  const [todo, createTodo] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,10 +14,21 @@ export default function TodoList({ currentUser }) {
     fetchData();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       TodoList
       <div>
+        <div>
+          {' '}
+          <form onSubmit={handleSubmit}>
+            <input name="todo" onChange={(e) => createTodo(e.target.value)} required />
+            <button>Add todo</button>
+          </form>
+        </div>
         {todos.map((todo) => (
           <p key={todo.id}>{todo.id}</p>
         ))}
